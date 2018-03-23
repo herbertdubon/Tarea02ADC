cddbffbf61f0046a29cb0459ab4e57accba7c85e
herbertdubon <hdubon@tecnasa.com>
2018-03-22 22:13:35 -0600

    Ejercicio 2 Calcular Peso Python

---
+++ Ejercicio 2 Calcular Peso Python/Peso.py
@@ -0,0 +1,10 @@
+# Fuerza de la Gravedad
+Gravedad = 9.8
+
+# Ingresar Masa
+Masa = float(input("Ingrese la masa de la máquina del tiempo en kg: "))
+
+# Calculando el peso de la maquina del tiempo
+Peso = Masa*Gravedad
+print("El peso de la máquina del tiempo es = " + format(Peso, ".2f")+ " kg*m/s^2")
+
